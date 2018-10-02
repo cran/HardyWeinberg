5353be5192fa8cb14f67de4c2d892189f500a87c
Himanshu Joshi <hihi.joshi@gmail.com>
2018-10-02 13:16:59 +1000

    Replaced recursive functions with while loops to avoid hitting recursion limits

--- R/CompProbDown.R
+++ R/CompProbDown.R
@@ -1,10 +1,15 @@
-CompProbDown <-
-function(nAA,nBB,EnAB,prob,vec=NULL) {
-  pr <-  prob*EnAB*(EnAB-1)/(4*(nAA+1)*(nBB+1))
-  nvec <- c(vec,pr)
-  if(EnAB > 3) {
-     nvec <- CompProbDown(nAA+1,nBB+1,EnAB-2,pr,nvec)
+CompProbDown <- function(nAA,nBB,EnAB,prob,vec=NULL) 
+{
+  prob <-  prob*EnAB*(EnAB-1)/(4*(nAA+1)*(nBB+1))
+  nvec <- c(vec, prob)
+  
+  while(EnAB > 3) {
+    nAA <- nAA + 1
+    nBB <- nBB + 1
+    EnAB <- EnAB - 2
+    prob <-  prob*EnAB*(EnAB-1)/(4*(nAA+1)*(nBB+1))
+    nvec <- c(nvec ,prob)
   }
+  
   return(nvec)
 }
-
